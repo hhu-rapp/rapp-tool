b4dd90fbed437feab9b889efaf25774973c7ec11
Manh Khoi Duong <20096623+mkduong-ai@users.noreply.github.com>
2021-08-03 18:57:29 +0200

    User can specify path destination of report file
    - Minor fixes

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 
 # standard library
 import sqlite3
+import os
 
 # common
 import numpy as np
@@ -44,45 +45,50 @@ class MLPipeline(object):
         self.parser.add('-cf', '--config-file', required=True, is_config_file=True, help='config file path')
 
         # parsing arguments from the config file
-        self.parser.add_argument("-f", "--filename", type=str, help="Location of the .db file.",
+        self.parser.add_argument('-f', '--filename', type=str, help='Location of the .db file.',
                                  required=True)
-        self.parser.add_argument("-s", "--sql_filename", type=str, help="Location of the sql query file.",
+        self.parser.add_argument('-s', '--sql_filename', type=str, help='Location of the sql query file.',
                                  required=True)
-        self.parser.add_argument("-l", "--label_name", type=str, help="Column name of the prediction label.",
+        self.parser.add_argument('-l', '--label_name', type=str, help='Column name of the prediction label.',
                                  required=True)
-        self.parser.add_argument("-c", "--categorical", nargs='+', help="List of categorical columns.",
+        self.parser.add_argument('-c', '--categorical', nargs='+', help='List of categorical columns.',
                                  required=True)
-        self.parser.add_argument("-t", "--type", type=str, default='classification',
-                                 help="classification or regression. Default: classification",
+        self.parser.add_argument('-t', '--type', type=str, default='classification',
+                                 choices=['classification', 'regression'],
+                                 help='classification or regression. Default: classification',
                                  required=True)
-        self.parser.add_argument("--imputation", type=str, default='iterative',
-                                 help="Imputation method for non-categorical data. Available: knn, iterative, mean, "
-                                      "median, most_frequent",
+        self.parser.add_argument('--imputation', type=str, default='iterative',
+                                 choices=['knn', 'iterative', 'mean', 'median', 'most_frequent'],
+                                 help='Imputation method for non-categorical data. Available: knn, iterative, mean, '
+                                      'median, most_frequent',
                                  required=False)
-        self.parser.add_argument("--feature_selection", type=str, default='variance',
-                                 help="Feature selection method to reduce the amount of features. Available: variance, "
-                                      "",
+        self.parser.add_argument('--feature_selection', type=str, default='variance',
+                                 choices=['variance'],
+                                 help='Feature selection method to reduce the amount of features. Available: variance, '
+                                      '',
                                  required=False)
-        self.parser.add_argument("--save_report", type=str, default="False",
-                                 help="Boolean value whether a report should be exported",
+        self.parser.add_argument('--save_report', type=str, default='True',
+                                 choices=['True', 'False'],
+                                 help='Boolean value whether a report should be exported',
                                  required=False)
-        self.parser.add_argument("--plot_confusion_matrix", type=str, default="False",
-                                 help="Boolean value whether to plot confusion matrices for each classifier. "
-                                      "Is only applied for classification.",
+        self.parser.add_argument('--report_path', type=str, default='',
+                                 help='Path destination of report file. If path is given, then the report file will '
+                                      'be saved in path/results_report.csv. Note: Relative path to working directory.')
+        self.parser.add_argument('--plot_confusion_matrix', type=str, default='False',
+                                 help='Boolean value whether to plot confusion matrices for each classifier. '
+                                      'Is only applied for classification.',
                                  required=False)
-
 
         self.args = self.parser.parse_args()
 
         self.args.plot_confusion_matrix = eval(self.args.plot_confusion_matrix)
-        print(self.args.plot_confusion_matrix)
         self.args.save_report = eval(self.args.save_report)
 
         # reading and quering database
         con = sqlite3.connect(self.args.filename)
         with open(self.args.sql_filename) as f:
             sql_query = f.readlines()
-            sql_query = "".join(sql_query)
+            sql_query = ''.join(sql_query)
         self.df = pd.read_sql_query(sql_query, con)
 
         # fill missing values
@@ -109,7 +115,7 @@ class MLPipeline(object):
         if self.args.type == 'classification':
             self.estimators = [RandomForestClassifier(), DecisionTreeClassifier(class_weight='balanced'),
                                SVC(), GaussianNB(), LogisticRegression()]
-        else:
+        elif self.args.type == 'regression':
             self.estimators = [LinearRegression(), ElasticNet(), BayesianRidge()]
         self.train_estimators()
 
@@ -117,9 +123,9 @@ class MLPipeline(object):
         self.tune_estimators()
 
         if self.args.type == 'classification':
-            self.scores = {'accuracy': [], 'f1': [], 'recall': [], 'precision': []}
+            self.scores = {'Accuracy': [], 'F1': [], 'Recall': [], 'Precision': []}
         else:
-            self.scores = {'mae': [], 'r2': [], 'mse': []}
+            self.scores = {'Mean Absolute Error': [], 'Mean Squared Error': [], 'R2': []}
         self.score_estimators()
 
         # Report results
@@ -132,24 +138,24 @@ class MLPipeline(object):
         if self.args.type == 'classification' and self.args.plot_confusion_matrix:
             self.plot_confusion_matrix()
 
-    def impute(self, method="iterative"):
+    def impute(self, method='iterative'):
         # Only impute categorical data
         self.df[self.args.categorical] = \
-            SimpleImputer(strategy="most_frequent").fit_transform(self.df[self.args.categorical])
+            SimpleImputer(strategy='most_frequent').fit_transform(self.df[self.args.categorical])
 
         # impute non-categorical data
-        if method == "knn":
-            imputer = KNNImputer(n_neighbors=5, weights="distance")
-        elif method == "iterative":
+        if method == 'knn':
+            imputer = KNNImputer(n_neighbors=5, weights='distance')
+        elif method == 'iterative':
             imputer = IterativeImputer()
-        elif method == "mean":
-            imputer = SimpleImputer(strategy="mean")
-        elif method == "median":
-            imputer = SimpleImputer(strategy="median")
-        elif method == "most_frequent":
-            imputer = SimpleImputer(strategy="most_frequent")
+        elif method == 'mean':
+            imputer = SimpleImputer(strategy='mean')
+        elif method == 'median':
+            imputer = SimpleImputer(strategy='median')
+        elif method == 'most_frequent':
+            imputer = SimpleImputer(strategy='most_frequent')
         else:
-            imputer = KNNImputer(n_neighbors=5, weights="distance")
+            imputer = KNNImputer(n_neighbors=5, weights='distance')
 
         self.df[self.df.columns.difference(self.args.categorical)] \
             = imputer.fit_transform(self.df[self.df.columns.difference(self.args.categorical)])
@@ -164,7 +170,7 @@ class MLPipeline(object):
         """
         self.X = pd.get_dummies(data=self.X, columns=self.args.categorical)
 
-    def feature_selection(self, method="variance"):
+    def feature_selection(self, method='variance'):
         """
         Selects the most important features based on the used strategy
 
@@ -173,7 +179,7 @@ class MLPipeline(object):
         None
         """
 
-        if method == "variance":
+        if method == 'variance':
             sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
             self.X = sel.fit_transform(self.X)
         else:
@@ -191,16 +197,16 @@ class MLPipeline(object):
         if self.args.type == 'classification':
             for i in range(len(self.estimators)):
                 y_pred = self.estimators[i].predict(self.X_val)
-                self.scores['accuracy'].append(sk.metrics.accuracy_score(self.y_val, y_pred))
-                self.scores['f1'].append(sk.metrics.f1_score(self.y_val, y_pred, average='weighted'))
-                self.scores['recall'].append(sk.metrics.recall_score(self.y_val, y_pred, average='weighted'))
-                self.scores['precision'].append(sk.metrics.precision_score(self.y_val, y_pred, average='weighted'))
-        else:
+                self.scores['Accuracy'].append(sk.metrics.accuracy_score(self.y_val, y_pred))
+                self.scores['F1'].append(sk.metrics.f1_score(self.y_val, y_pred, average='weighted'))
+                self.scores['Recall'].append(sk.metrics.recall_score(self.y_val, y_pred, average='weighted'))
+                self.scores['Precision'].append(sk.metrics.precision_score(self.y_val, y_pred, average='weighted'))
+        elif self.args.type == 'regression':
             for i in range(len(self.estimators)):
                 y_pred = self.estimators[i].predict(self.X_val)
-                self.scores['mae'].append(sk.metrics.mean_absolute_error(self.y_val, y_pred))
-                self.scores['mse'].append(sk.metrics.mean_squared_error(self.y_val, y_pred))
-                self.scores['r2'].append(sk.metrics.r2_score(self.y_val, y_pred))
+                self.scores['Mean Absolute Error'].append(sk.metrics.mean_absolute_error(self.y_val, y_pred))
+                self.scores['Mean Squared Error'].append(sk.metrics.mean_squared_error(self.y_val, y_pred))
+                self.scores['R2'].append(sk.metrics.r2_score(self.y_val, y_pred))
 
     def tune_estimators(self):
         pass
@@ -262,8 +268,16 @@ class MLPipeline(object):
                ...
             }
         """
-        # TODO: User given path for export. Only export if path is specified.
-        report_df.to_csv('classification_report.csv', index=True)
+        filename = 'results_report.csv'
+        if len(self.args.report_path) > 0:
+            # create directory if it does not exist
+            if not os.path.exists(self.args.report_path):
+                os.makedirs(self.args.report_path)
+
+            # export file
+            report_df.to_csv(self.args.report_path+'/'+filename, index=True)
+        elif self.args.save_report:
+            report_df.to_csv(filename, index=True)
 
     def plot_confusion_matrix(self):
         for i in range(len(self.estimators)):
