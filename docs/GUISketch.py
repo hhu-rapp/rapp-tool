21a7bc28579b645db4536aa81787bc31d476cf71
Manh Khoi Duong <maduo100@hhu.de>
2021-08-05 14:32:47 +0200

    Structure for GUI

---
+++ docs/GUISketch.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'GUI.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 24))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        self.menuEdit = QtWidgets.QMenu(self.menubar)
+        self.menuEdit.setObjectName("menuEdit")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionOpen_Database = QtWidgets.QAction(MainWindow)
+        self.actionOpen_Database.setObjectName("actionOpen_Database")
+        self.actionCopy = QtWidgets.QAction(MainWindow)
+        self.actionCopy.setObjectName("actionCopy")
+        self.actionPaste = QtWidgets.QAction(MainWindow)
+        self.actionPaste.setObjectName("actionPaste")
+        self.actionOpen_SQLite_Query = QtWidgets.QAction(MainWindow)
+        self.actionOpen_SQLite_Query.setObjectName("actionOpen_SQLite_Query")
+        self.menuFile.addAction(self.actionOpen_Database)
+        self.menuFile.addAction(self.actionOpen_SQLite_Query)
+        self.menuEdit.addAction(self.actionCopy)
+        self.menuEdit.addAction(self.actionPaste)
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuEdit.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.menuEdit.setTitle(_translate("MainWindow", "Edit"))
+        self.actionOpen_Database.setText(_translate("MainWindow", "Open Database"))
+        self.actionOpen_Database.setStatusTip(_translate("MainWindow", "Opens SQLite Database. File type is \'.db\'"))
+        self.actionOpen_Database.setShortcut(_translate("MainWindow", "Ctrl+O"))
+        self.actionCopy.setText(_translate("MainWindow", "Copy"))
+        self.actionPaste.setText(_translate("MainWindow", "Paste"))
+        self.actionOpen_SQLite_Query.setText(_translate("MainWindow", "Open SQLite Query"))
+        self.actionOpen_SQLite_Query.setStatusTip(_translate("MainWindow", "Opens an SQLite query file"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
--- src/GUI.py
+++ src/GUI.py
@@ -1,7 +1,81 @@
-# importing the required libraries
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+# internal Python packages
 import sys
+import sqlite3
+
+# PyQt5
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+# dataframe
+import pandas as pd
+from PandasModel import PandasModel
+
+
+class DataFrameModel(QtCore.QAbstractTableModel):
+    """
+    From stackoverflow:
+    https://stackoverflow.com/questions/44603119/how-to-display-a-pandas-data-frame-with-pyqt5-pyside2
+    """
+    DtypeRole = QtCore.Qt.UserRole + 1000
+    ValueRole = QtCore.Qt.UserRole + 1001
+
+    def __init__(self, df=pd.DataFrame(), parent=None):
+        super(DataFrameModel, self).__init__(parent)
+        self._dataframe = df
+
+    def setDataFrame(self, dataframe):
+        self.beginResetModel()
+        self._dataframe = dataframe.copy()
+        self.endResetModel()
+
+    def dataFrame(self):
+        return self._dataframe
+
+    dataFrame = QtCore.pyqtProperty(pd.DataFrame, fget=dataFrame, fset=setDataFrame)
+
+    @QtCore.pyqtSlot(int, QtCore.Qt.Orientation, result=str)
+    def headerData(self, section: int, orientation: QtCore.Qt.Orientation, role: int = QtCore.Qt.DisplayRole):
+        if role == QtCore.Qt.DisplayRole:
+            if orientation == QtCore.Qt.Horizontal:
+                return self._dataframe.columns[section]
+            else:
+                return str(self._dataframe.index[section])
+        return QtCore.QVariant()
+
+    def rowCount(self, parent=QtCore.QModelIndex()):
+        if parent.isValid():
+            return 0
+        return len(self._dataframe.index)
+
+    def columnCount(self, parent=QtCore.QModelIndex()):
+        if parent.isValid():
+            return 0
+        return self._dataframe.columns.size
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if not index.isValid() or not (0 <= index.row() < self.rowCount() \
+                                       and 0 <= index.column() < self.columnCount()):
+            return QtCore.QVariant()
+        row = self._dataframe.index[index.row()]
+        col = self._dataframe.columns[index.column()]
+        dt = self._dataframe[col].dtype
+
+        val = self._dataframe.iloc[row][col]
+        if role == QtCore.Qt.DisplayRole:
+            return str(val)
+        elif role == DataFrameModel.ValueRole:
+            return val
+        if role == DataFrameModel.DtypeRole:
+            return dt
+        return QtCore.QVariant()
+
+    def roleNames(self):
+        roles = {
+            QtCore.Qt.DisplayRole: b'display',
+            DataFrameModel.DtypeRole: b'dtype',
+            DataFrameModel.ValueRole: b'value'
+        }
+        return roles
 
 
 class Window(QMainWindow):
@@ -9,9 +83,10 @@ class Window(QMainWindow):
         super().__init__()
 
         self.initUI()
-        self.initMenu() # TODO Menubar
-        #self.initWidgets()
-        # show all the widgets
+        self.initMenu()
+        self.retranslateUi()
+        self.initMenuAction()
+
         self.show()
 
     def initUI(self):
@@ -19,25 +94,96 @@ class Window(QMainWindow):
         self.setWindowTitle('Responsible Performance Prediction [Demoversion]')
 
         # setting  the geometry of window
-        # setGeometry(left, top, width, height)
-        self.setGeometry(100, 60, 1280, 720)
+        self.width = 1280
+        self.height = 720
+        self.setGeometry(100, 60, self.width, self.height)
 
     def initMenu(self):
-        # create actions for the menubar
-        openSQLDBAction = QAction('& Load database', self)
-        openSQLDBAction.setStatusTip('Opening a .sql database file.')
-        openSQLDBAction.triggered.connect(self.test_action)
+        #self.vLayout = QtWidgets.QVBoxLayout(self)
+        #self.hLayout = QtWidgets.QHBoxLayout()
 
-        mainMenu = self.menuBar()
-        fileMenu = mainMenu.addMenu('&File')
-        fileMenu.addAction(openSQLDBAction)
+        # Menubar
+        self.menubar = QtWidgets.QMenuBar(self)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, self.width, 24))
+        self.menubar.setObjectName("menubar")
 
-    def test_action(self):
-        print('Action triggered')
+        # Menu
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        self.menuEdit = QtWidgets.QMenu(self.menubar)
+        self.menuEdit.setObjectName("menuEdit")
+
+        self.setMenuBar(self.menubar)
+
+        # status bar
+        self.statusbar = QtWidgets.QStatusBar(self)
+        self.statusbar.setObjectName("statusbar")
+        self.setStatusBar(self.statusbar)
+
+        # setting actions for the menu
+        # File
+        self.actionOpen_Database = QtWidgets.QAction(self)
+        self.actionOpen_Database.setObjectName("actionOpen_Database")
+        self.actionOpen_SQLite_Query = QtWidgets.QAction(self)
+        self.actionOpen_SQLite_Query.setObjectName("actionOpen_SQLite_Query")
+
+        # Edit
+        self.actionCopy = QtWidgets.QAction(self)
+        self.actionCopy.setObjectName("actionCopy")
+        self.actionPaste = QtWidgets.QAction(self)
+        self.actionPaste.setObjectName("actionPaste")
+
+        # add entries to the menu
+        self.menuFile.addAction(self.actionOpen_Database)
+        self.menuFile.addAction(self.actionOpen_SQLite_Query)
+        self.menuEdit.addAction(self.actionCopy)
+        self.menuEdit.addAction(self.actionPaste)
+
+        # adding to menubar
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuEdit.menuAction())
+
+        # Table view
+        self.pandasTv = QtWidgets.QTableView(self)
+        self.pandasTv.move(0, 24)
+
+        # arrange layout
+        #self.vLayout.addWidget(self.menubar)
+        #self.vLayout.addWidget(self.pandasTv)
+
+    def retranslateUi(self):
+        _translate = QtCore.QCoreApplication.translate
+        self.setWindowTitle(_translate("Window", "MainWindow"))
+        self.menuFile.setTitle(_translate("Window", "File"))
+        self.menuEdit.setTitle(_translate("Window", "Edit"))
+
+        self.actionOpen_Database.setText(_translate("Window", "Open Database"))
+        self.actionOpen_Database.setStatusTip(_translate("Window", "Opens SQLite Database. File type is \'.db\'"))
+        self.actionOpen_Database.setShortcut(_translate("Window", "Ctrl+O"))
+
+        self.actionOpen_SQLite_Query.setText(_translate("Window", "Open SQLite Query"))
+        self.actionOpen_SQLite_Query.setStatusTip(_translate("Window", "Opens an SQLite query file"))
+
+        self.actionCopy.setText(_translate("Window", "Copy"))
+        self.actionPaste.setText(_translate("Window", "Paste"))
+
+    def initMenuAction(self):
+        self.actionOpen_Database.triggered.connect(self.openDatabase)
+
+    def openDatabase(self):
+        print('Opening Database')
+        self.filepath_db = QtWidgets.QFileDialog.getOpenFileName()[0]
+        con = sqlite3.connect(self.filepath_db)
+        df = pd.read_sql_query('SELECT * FROM Einschreibung', con)
+        #df = pd.read_sql(con)
+        # df = pd.read_csv(self.filepath_db)
+        model = PandasModel(df)
+        self.pandasTv.setModel(model)
+        self.pandasTv.resizeColumnsToContents()
 
     def initWidgets(self):
         # creating a label widget
-        self.widget = QLabel('No database loaded.', self)
+        self.widget = QtWidgets.QLabel('No database loaded.', self)
 
 
 if __name__ == '__main__':
