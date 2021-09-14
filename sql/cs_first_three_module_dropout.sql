4483fbf5b9524762c212d69b3f1c4ae8939ec917
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2021-09-14 12:16:23 +0200

    Expand CS dropout analysis to first two/three modules

---
+++ sql/cs_first_three_module_dropout.sql
@@ -0,0 +1,146 @@
+-- Features:
+--  Binary markers whether Linear Algebra, Programming (Info 1), and
+--  Algorithms (Info 3) where passed and how many attempts where used.
+--  Last achieved grade in Programming and Algorithms. Linear Algebra does not
+--  have any recorded grades for CS students besides 0.0 (passed) and 5.0
+--  (failed).
+-- Target variables:
+--  Number of credits in fourth term
+-- Note:
+--  In 2018 PO computer science switched what the first term modules are for
+--  students, and to be more relevant for future data, we consider these new
+--  three here: linear algebra, programming, and algorithms
+SELECT
+  S.Geburtsjahr,
+  S.Geschlecht,
+  S.Deutsch,
+  strftime("%Y", E.Immatrikulationsdatum) - S.Geburtsjahr as AlterEinschreibung,
+  case when LA.Bestanden IS NOT NULL then LA.Bestanden else 0 end as LABestanden,
+  case when LA.Versuche IS NOT NULL then LA.Versuche else 0 end as LAVersuche,
+  -- We do not have a grade for linear algebra I.
+  case when Prog.Bestanden IS NOT NULL then Prog.Bestanden else 0 end as ProgBestanden,
+  case when Prog.Versuche IS NOT NULL then Prog.Versuche else 0 end as ProgVersuche,
+  case when ProgNote.Note IS NOT NULL then ProgNote.Note else 5. end as ProgNote,
+  case when Algo.Bestanden IS NOT NULL then Algo.Bestanden else 0 end as AlgoBestanden,
+  case when Algo.Versuche IS NOT NULL then Algo.Versuche else 0 end as AlgoVersuche,
+  case when AlgoNote.Note IS NOT NULL then AlgoNote.Note else 5. end as AlgoNote,
+  Dropout.Dropout
+FROM
+  Student as S,
+  Student_schreibt_Pruefung as SSP,
+  Pruefung as P,
+  Einschreibung as E
+LEFT JOIN
+  ( SELECT
+      -- SSP.Note as Note,
+      max(CASE WHEN SSP.Status = 'bestanden' then 1 else 0 end) as Bestanden,
+      max(SSP.Versuch) as Versuche,
+      SSP.Pseudonym
+    FROM
+      Student_schreibt_Pruefung as SSP,
+      Pruefung as P
+    WHERE P.Nummer = SSP.Nummer
+      AND P.Version = SSP.Version
+      AND P.Modul IN ('Lineare Algebra', 'Lineare Algebra I')
+    GROUP BY SSP.Pseudonym
+    ) as LA
+  ON LA.Pseudonym = SSP.Pseudonym
+LEFT JOIN
+  ( SELECT
+      max(CASE WHEN SSP.Status = 'bestanden' then 1 else 0 end) as Bestanden,
+      max(SSP.Versuch) as Versuche,
+      SSP.Pseudonym
+    FROM
+      Student_schreibt_Pruefung as SSP,
+      Pruefung as P
+    WHERE P.Nummer = SSP.Nummer
+      AND P.Version = SSP.Version
+      AND P.Modul IN ('Programmierung ', 'Grundlagen der Softwareentwicklung und Programmierung')
+    GROUP BY SSP.Pseudonym
+    ) as Prog
+  ON Prog.Pseudonym = SSP.Pseudonym
+LEFT JOIN
+  ( SELECT
+      SSP.Versuch,
+	    SSP.Note,
+      SSP.Pseudonym
+    FROM
+      Student_schreibt_Pruefung as SSP,
+      Pruefung as P
+    WHERE P.Nummer = SSP.Nummer
+      AND P.Version = SSP.Version
+      AND P.Modul IN ('Programmierung ', 'Grundlagen der Softwareentwicklung und Programmierung')
+    ORDER BY SSP.Pseudonym
+    ) as ProgNote
+  ON Prog.Pseudonym = ProgNote.Pseudonym AND Prog.Versuche = ProgNote.Versuch
+LEFT JOIN
+  ( SELECT
+      max(CASE WHEN SSP.Status = 'bestanden' then 1 else 0 end) as Bestanden,
+      max(SSP.Versuch) as Versuche,
+      SSP.Pseudonym
+    FROM
+      Student_schreibt_Pruefung as SSP,
+      Pruefung as P
+    WHERE P.Nummer = SSP.Nummer
+      AND P.Version = SSP.Version
+      AND P.Modul IN ('Algorithmen und Datenstrukturen', 'Grundlagen der Algorithmen und Datenstrukturen')
+    GROUP BY SSP.Pseudonym
+    ) as Algo
+  ON Algo.Pseudonym = SSP.Pseudonym
+LEFT JOIN
+  ( SELECT
+      SSP.Versuch,
+	    SSP.Note,
+      SSP.Pseudonym
+    FROM
+      Student_schreibt_Pruefung as SSP,
+      Pruefung as P
+    WHERE P.Nummer = SSP.Nummer
+      AND P.Version = SSP.Version
+      AND P.Modul IN ('Algorithmen und Datenstrukturen', 'Grundlagen der Algorithmen und Datenstrukturen')
+    ORDER BY SSP.Pseudonym
+    ) as AlgoNote
+  ON Algo.Pseudonym = AlgoNote.Pseudonym AND Algo.Versuche = AlgoNote.Versuch
+INNER JOIN
+  (SELECT -- dropout_no_exam_last_three_terms.sql
+    S.Pseudonym,
+    E.Studienfach,
+    E.Abschluss,
+    -- E.Bestanden,
+    -- LetztePruefung.SemesterCode,
+    CASE WHEN LetztePruefung.SemesterCode < 20192 THEN NOT E.Bestanden ELSE null END as Dropout
+    -- 20192 is the code for wintersemester 2019 (summersemester would be 20191).
+    -- This is three terms ago, given the age of the dataset.
+    -- Thus, everybody not writing an exam in the last three terms is assumed
+    -- to have ended their studyship, and those whom did not graduate are deemed
+    -- as dropouts.
+  FROM
+    Student as S,
+    Einschreibung as E
+  JOIN
+    (SELECT
+      SSP.Pseudonym,
+      max(SSP.Semesterjahr*10 + (2-SSP.Sommersemester)) as SemesterCode
+    FROM
+      Student_schreibt_Pruefung as SSP
+    GROUP BY SSP.Pseudonym
+    ) as LetztePruefung
+    ON S.Pseudonym = LetztePruefung.Pseudonym
+  WHERE S.Pseudonym = E.Pseudonym
+  ) as Dropout ON Dropout.Pseudonym = S.Pseudonym
+WHERE S.Pseudonym = SSP.Pseudonym
+  AND S.Pseudonym = E.Pseudonym
+  AND SSP.Version = P.Version
+  AND SSP.Nummer = P.Nummer
+  AND E.Studienfach = "Informatik"
+  AND E.Abschluss = "Bachelor"
+  AND SSP.Studienfach = E.Studienfach
+  AND SSP.Abschluss = E.Abschluss
+  AND Dropout.Studienfach = E.Studienfach
+  AND Dropout.Abschluss = E.Abschluss
+  -- Make sure we only have students which took part in at least one of these
+  AND NOT (LA.Versuche IS NULL AND Prog.Versuche IS NULL AND Algo.Versuche IS NULL)
+  -- Only consider since PO 2007
+  AND '2007-01-01' <= date(E.Immatrikulationsdatum)
+  AND Dropout.Dropout IN (0, 1) -- NULL-Entries are sorted out
+GROUP BY S.Pseudonym
