af0cff44bc0edc8b42a90446b7d92d82e9910494
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2021-10-08 13:00:43 +0200

    Finish analysis for CS fourth semester success prediction

--- rapp/fairness.py
+++ rapp/fairness.py
@@ -75,7 +75,7 @@ def clf_group_fairness(clf, data, group_names):
     return group_fairness(test_set, group_names, outcol)
 
 
-def predictive_parity(df, group_names, ground_truth, outcome_name):
+def predictive_equality(df, group_names, ground_truth, outcome_name):
     """
     Assesses the fairness in the dataframe over the given groups.
     Returns a map containing an entry for each specified group in `group_names`.
@@ -102,9 +102,8 @@ def predictive_parity(df, group_names, ground_truth, outcome_name):
     for group in group_names:
         fair = {}
 
-        priv_mask = df[group] == 1
-        priv = df[df[ground_truth]==1][priv_mask]
-        unpriv = df[df[ground_truth]==1][~priv_mask]
+        priv = df[(df[group] == 1) & (df[ground_truth]==0)]
+        unpriv = df[(df[group] != 1) & (df[ground_truth]==0)]
 
         priv_fav = len(priv[priv[outcome_name] == 1])
         priv_unfav = len(priv[priv[outcome_name] != 1])
@@ -122,7 +121,7 @@ def predictive_parity(df, group_names, ground_truth, outcome_name):
     return fairnesses
 
 
-def clf_predictive_parity(clf, data, ground_truth, group_names):
+def clf_predictive_equality(clf, data, ground_truth, group_names):
     """
     Assesses the fairness of the give classiier over the dataframe per  group.
     Returns a map containing an entry for each specified group in `group_names`.
@@ -151,4 +150,83 @@ def clf_predictive_parity(clf, data, ground_truth, group_names):
 
     test_set[outcol] = clf.predict(test_set.drop([ground_truth], axis=1)) # Predictions
 
-    return predictive_parity(test_set, group_names, ground_truth, outcol)
+    return predictive_equality(test_set, group_names, ground_truth, outcol)
+
+
+def equality_of_opportunity(df, group_names, ground_truth, outcome_name):
+    """
+    Assesses the fairness in the dataframe over the given groups.
+    Returns a map containing an entry for each specified group in `group_names`.
+    For a group G, the entries are:
+
+        result[G] = {
+            0: {"favourable_outcome": number_unpriviliged_with_fav_outcome,
+                "favourable_outcome": number_unpriviliged_with_fav_outcome},
+            1: {"favourable_outcome": number_priviliged_with_fav_outcome,
+                "favourable_outcome": number_priviliged_with_fav_outcome},
+            "disparity": statistical_disparity_over_priv_and_unpriv_group
+        }
+
+    df: A pandas dataframe ccontaining the column `outcome_name` and each of the
+        listed `group_names`.
+    group_names: A list of column names defining sensitive attributes.
+    ground_truth: Name of the column corresponding to the ground truth value.
+        Assumes 1 as favourable outcome and 0 as unfavourable outcome.
+    outcome_name: Name of the column corresponding to the outcome. Assumes 1 as
+        favourable outcome and 0 as unfavourable outcome.
+    """
+
+    fairnesses = {}
+    for group in group_names:
+        fair = {}
+
+        priv = df[(df[group] == 1) & (df[ground_truth]==1)]
+        unpriv = df[(df[group] != 1) & (df[ground_truth]==1)]
+
+        priv_fav = len(priv[priv[outcome_name] == 1])
+        priv_unfav = len(priv[priv[outcome_name] != 1])
+        unpriv_fav = len(unpriv[unpriv[outcome_name] == 1])
+        unpriv_unfav = len(unpriv[unpriv[outcome_name] != 1])
+
+        fair[1] = {"favourable_outcome": priv_fav,
+                   "unfavourable_outcome": priv_unfav}
+        fair[0] = {"favourable_outcome": unpriv_fav,
+                   "unfavourable_outcome": unpriv_unfav}
+        fair["disparity"] = priv_unfav/len(priv) - unpriv_unfav/len(unpriv)
+
+        fairnesses[group] = fair
+
+    return fairnesses
+
+
+
+def clf_equality_of_opportunity(clf, data, ground_truth, group_names):
+    """
+    Assesses the fairness of the give classiier over the dataframe per  group.
+    Returns a map containing an entry for each specified group in `group_names`.
+    For a group G, the entries are:
+
+        result[G] = {
+            0: {"favourable_outcome": number_unpriviliged_with_fav_outcome,
+                "favourable_outcome": number_unpriviliged_with_fav_outcome},
+            1: {"favourable_outcome": number_priviliged_with_fav_outcome,
+                "favourable_outcome": number_priviliged_with_fav_outcome},
+            "disparity": statistical_disparity_over_priv_and_unpriv_group
+        }
+
+    clf: A classifier with a `predict()` function.
+    data: A pandas dataframe containing each of the listed `group_names`.
+    group_names: A list of column names defining sensitive attributes.
+    ground_truth:
+        Assumes 1 as favourable outcome and 0 as unfavourable outcome.
+    """
+    test_set = data.copy(deep=True)
+
+    outcol = "Outcome"
+    cols = test_set.columns
+    while outcol in cols:
+        outcol += "_" # We add _ as long as we need to to get a unique name.
+
+    test_set[outcol] = clf.predict(test_set.drop([ground_truth], axis=1)) # Predictions
+
+    return equality_of_opportunity(test_set, group_names, ground_truth, outcol)
