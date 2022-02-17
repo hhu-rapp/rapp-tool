0cb07726426f5f2507945c2bae0c15b86f82afde
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2022-02-17 14:51:39 +0100

    Update parsing

--- rapp/parser.py
+++ rapp/parser.py
@@ -7,10 +7,10 @@ import configargparse
 class RappConfigParser(object):
 
     def __init__(self):
-        self.parser = self.setup_parser()
+        self.parser = self._setup_parser()
 
-    def parse_args(self):
-        parsed = self.parser.parse_args()
+    def parse_args(self, args):
+        parsed = self.parser.parse_args(args=args)
 
         # When `estimators` config is not given, we default based on type.
         if parsed.estimators is None:
@@ -19,17 +19,33 @@ class RappConfigParser(object):
             elif parsed.type == 'regression':
                 parsed.estimators = ['LR', 'EL', 'BR']
 
+        # Double Check that the SQL-information was given correctly.
+        has_arg = lambda arg: parsed.__dict__[arg] is not None  # Helper fun.
+        uses_templating = (has_arg('studies_id') or
+                           has_arg('features_id') or
+                           has_arg('labels_id'))
+        if uses_templating:
+            if (not (has_arg('studies_id')
+                     and has_arg('features_id')
+                     and has_arg('labels_id'))):
+                raise Exception("When using either --studies_id, --features_id,"
+                                " or --labels_id, all three must be given.")
+            # Cannot use --sql_file now.
+            if has_arg('sql_file') or has_arg('sql_query'):
+                raise Exception("Can only have either sql file, or templating"
+                                " engine, or sql query, not multiple.")
+        elif has_arg('sql_file') and has_arg('sql_query'):
+            raise Exception("Can only have either sql file, or templating"
+                            " engine, or sql query, not multiple.")
+        elif not (has_arg('sql_file') or has_arg('sql_query')):
+            raise Exception("Must have information about which SQL query"
+                            " to use.")
+
         return parsed
 
-    def setup_parser(self):
-        """
-
-        Returns
-        -------
-        ArgParser
-        """
+    def _setup_parser(self):
         parser = configargparse.ArgParser()
-        parser.add('-cf', '--config-file', required=True,
+        parser.add('-cf', '--config-file', required=False,
                    is_config_file=True, help='config file path')
 
         # methodical settings
@@ -37,15 +53,31 @@ class RappConfigParser(object):
                             help='Location of the .db file.',
                             required=True)
 
+        # The SQL settings need to be manually be cross-checked that they
+        # are correctly assessed.
+        # The following settings are mutually exclusive
+        #   --sql_file
+        #   --studies_id --features_id --labels_id
+        # Note that the logic behind argparse's add_mutually_exclusive_group
+        # does not allow for nested groups - which we need here though.
+
+        parser.add_argument('-sf', '--sql_file', type=str,
+                            help='Path to a custom SQL query file.',
+                            required=False)
+
+        parser.add_argument('-sq', '--sql_query', type=str,
+                            help='SQL query',
+                            required=False)
+
         parser.add_argument('-sid', '--studies_id', type=str,
                             help='Study Id for the sql query file.',
-                            required=True)
+                            required=False)
         parser.add_argument('-fid', '--features_id', type=str,
                             help='Feature Id for the sql query file.',
-                            required=True)
+                            required=False)
         parser.add_argument('-lid', '--labels_id', type=str,
                             help='Label Id for the sql query file.',
-                            required=True)
+                            required=False)
 
         parser.add_argument('-l', '--label_name', type=str,
                             help='Column name of the prediction label.',
@@ -81,7 +113,7 @@ class RappConfigParser(object):
                             choices=['classification', 'regression'],
                             help='classification or regression. Default: classification',
                             required=False)
-        parser.add_argument('--estimators', type=list(str), default=None,
+        parser.add_argument('--estimators', nargs='+', default=None,
                             required=False,
                             help='If given then --type is ignored.'
                             'Takes a single classifier to train on.')
