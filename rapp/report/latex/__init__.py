825f433244f3098e05ecc656bf4959d5ccb079cd
Jose <jose.cordova@uni-duesseldorf.de>
2022-04-08 12:01:11 +0200

    Refactor regression plots mustache  structure

--- rapp/report/latex/__init__.py
+++ rapp/report/latex/__init__.py
@@ -114,21 +114,39 @@ def tex_dataset_report(report):
 
 
 def tex_dataset_plot(report):
+    """
+    Parameters
+    ----------
+    report : dict[mode -> statistics]
+        Dictionary with mode as key which map onto calculated statistics
+        over a dataset.
+        The statistics are assumed to be in the following format:
+
+            {'groups':
+                {group1: {subgroup1: {'total': int,
+                                      'outcomes': {label1: int,
+                                                   label2: int,
+                                                   ...}},
+                          subgroup2: {...},
+                          ...},
+                 group2: {...},
+                 ...},
+             'outcomes': {label1: int, label2: int, ...},
+             'total': int}
+
+    Returns
+    -------
+    tex : str
+        Histogram displaying the regression statistics of the dataset.
+    """
 
     # Tex file expects the following format for Chevron per mode
     # {'total': int,
-    #  'groups: list({'group_name': str,
-    #                 'group_size': int,
-    #                 'start_column': int,
-    #                 'end_column': int,
-    #                 'subgroups': list({'sub_name': str, 'sub_count': int})}),
-    #  'labels': list({'label': any,
-    #                  'label_total': int,
-    #                  'label_groups': list({
-    #                    'label_group_total': int,
-    #                    'label_subgroups': list({
-    #                      'label_sub_name': string,
-    #                      'label_sub_count})})})}
+    #  'groups': list({'group_name': str,
+    #                  'subgroups': list({'sub_name': str,
+    #                                      'sub_count': int,
+    #                                      'sub_labels': list({'sub_label': any}, ...)}, ...)}, ...),
+    #  'labels': list({'label': any}, ...)}
 
     mustache = {'modes': []}
     for mode in report:
@@ -145,44 +163,25 @@ def tex_dataset_plot(report):
                     "sub_name": sub,
                     "sub_count": dataset["groups"][group][sub]["total"]
                 }
+                single_sub_labels = []
+                for sub_label in dataset["groups"][group][sub]["outcomes"].keys():
+                    for i in range(dataset["groups"][group][sub]["outcomes"][sub_label]):
+                        single_sub_labels.append({"sub_label": sub_label})
+
+                sub_data["sub_labels"] = single_sub_labels
                 group_data["subgroups"].append(sub_data)
-            group_data["size"] = len(group_data["subgroups"])
-            group_data["start_column"] = start_col_counter
-            group_data["end_column"] = start_col_counter + \
-                group_data["size"] - 1
-            start_col_counter = group_data["end_column"] + 1
             groups.append(group_data)
 
         # Fill the label data
-        labels = []
+        single_labels = []
         for label in dataset["outcomes"].keys():
-            unique_labels = []
             for i in range(dataset["outcomes"][label]):
-                label_data = {'label': label,
-                              'label_total': dataset["outcomes"][label],
-                              'label_groups': []}
-                for group_data in groups:
-                    group = group_data["group_name"]
-                    label_group_data = {
-                        'label_group_total': 0,
-                        'label_subgroups': []}
-                    for sub_data in group_data["subgroups"]:
-                        sub = sub_data['sub_name']
-                        sub_count = dataset["groups"][group][sub]["outcomes"][label]
-                        label_group_data["label_group_total"] += sub_count
-                        label_sub_data = {
-                            'label_sub_name': sub,
-                            'label_sub_count': sub_count,
-                        }
-                        label_group_data["label_subgroups"].append(
-                            label_sub_data)
-                    label_data["label_groups"].append(label_group_data)
-                labels.append(label_data)
+                single_labels.append({"label": label})
 
         mode_data = {"mode": mode.capitalize(),
                      "total": dataset["total"],
                      "groups": groups,
-                     "labels": labels}
+                     "labels": single_labels}
         mustache["modes"].append(mode_data)
 
     template = rc.get_text("dataset_plots.tex")
