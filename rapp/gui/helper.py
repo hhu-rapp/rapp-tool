0a8540db8ba74e45eea35a76de9cfe467bc72ad9
Jose <jose.cordova@uni-duesseldorf.de>
2022-06-25 18:08:29 +0200

    Add collapsible box widget

--- rapp/gui/helper.py
+++ rapp/gui/helper.py
@@ -156,3 +156,79 @@ class ClickableLabel(QtWidgets.QLabel):
 
     def set_click_function(self, function):
         self.mouseReleaseEvent = (lambda i: function(self.id))
+
+
+class CollapsibleBox(QtWidgets.QWidget):
+    """
+    Collapsible Box widget
+    Code from:
+    https://stackoverflow.com/questions/52615115/how-to-create-collapsible-box-in-pyqt
+    """
+    def __init__(self, title="", parent=None):
+        super(CollapsibleBox, self).__init__(parent)
+
+        self.toggle_button = QtWidgets.QToolButton(
+            text=title, checkable=True, checked=False
+        )
+        self.toggle_button.setStyleSheet("QToolButton { border: none; }")
+        self.toggle_button.setToolButtonStyle(
+            QtCore.Qt.ToolButtonTextBesideIcon
+        )
+        self.toggle_button.setArrowType(QtCore.Qt.RightArrow)
+        self.toggle_button.pressed.connect(self.on_pressed)
+
+        self.toggle_animation = QtCore.QParallelAnimationGroup(self)
+
+        self.content_area = QtWidgets.QScrollArea(
+            maximumHeight=0, minimumHeight=0
+        )
+        self.content_area.setSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed
+        )
+        self.content_area.setFrameShape(QtWidgets.QFrame.NoFrame)
+
+        vBoxLayout = QtWidgets.QVBoxLayout(self)
+        vBoxLayout.setContentsMargins(0, 0, 0, 0)
+        vBoxLayout.addWidget(self.toggle_button)
+        vBoxLayout.addWidget(self.content_area)
+
+        self.toggle_animation.addAnimation(
+            QtCore.QPropertyAnimation(self, b"minimumHeight")
+        )
+        self.toggle_animation.addAnimation(
+            QtCore.QPropertyAnimation(self, b"maximumHeight")
+        )
+        self.toggle_animation.addAnimation(
+            QtCore.QPropertyAnimation(self.content_area, b"maximumHeight")
+        )
+
+    @QtCore.pyqtSlot()
+    def on_pressed(self):
+        checked = self.toggle_button.isChecked()
+        self.toggle_animation.setDirection(
+            QtCore.QAbstractAnimation.Forward
+            if not checked
+            else QtCore.QAbstractAnimation.Backward
+        )
+        self.toggle_button.setArrowType(
+            QtCore.Qt.DownArrow if not checked else QtCore.Qt.RightArrow
+        )
+        self.toggle_animation.start()
+
+    def setContentLayout(self, layout):
+        self.content_area.setLayout(layout)
+        collapsed_height = (
+            self.sizeHint().height() - self.content_area.maximumHeight()
+        )
+        content_height = layout.sizeHint().height()
+        for i in range(self.toggle_animation.animationCount()-1):
+            animation = self.toggle_animation.animationAt(i)
+            animation.setDuration(100)
+            animation.setStartValue(collapsed_height)
+            animation.setEndValue(collapsed_height + content_height)
+
+        content_animation = self.toggle_animation.animationAt(
+                self.toggle_animation.animationCount()-1)
+        content_animation.setDuration(100)
+        content_animation.setStartValue(0)
+        content_animation.setEndValue(content_height)
