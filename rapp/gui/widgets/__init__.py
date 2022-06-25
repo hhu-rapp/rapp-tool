81f9c244eb87caf3f0df0e27d5c31cb53dd4bed6
Jose <jose.cordova@uni-duesseldorf.de>
2022-06-25 11:57:45 +0200

    Add individual fairness table as widget
    Also add a correspondence table to fairness section, and reimplement the fairness table to look as requested in Issue #52.

--- rapp/gui/widgets/__init__.py
+++ rapp/gui/widgets/__init__.py
@@ -374,3 +374,171 @@ class PerformanceMetricsTable(QtWidgets.QGroupBox):
             labelValue.setText(f"{value:.3f}")
             tableGridLayout.addWidget(labelValue, j + 1, 1, alignment=Qt.AlignCenter)
 
+
+class IndividualFairnessTable(QtWidgets.QGroupBox):
+    def __init__(self, data, model, fairness_results, sensitive_attribute, pl_type='classification'):
+        """
+        Generates a table with the performance and fairness result values, for a specific mode and sensitive attribute, 
+        for each model.
+
+        Parameters
+        ----------
+        data: rapp.pipeline.data object
+            Data used in the pipeline object (used to extract the modes)
+
+        model: Scikit-learn estimator
+            Selected individual estimator.
+
+        fairness_results: dict
+            Performance results with the form of: rapp.pipeline.performance_results.
+
+        sensitive_attribute: list
+            Sensitive attributes used in the pipeline.
+
+        pl_type: {'classification', 'regression}, default : 'classification'
+            Which type of prediction task is tackled by the pipeline.
+        """
+
+        super(IndividualFairnessTable, self).__init__()
+
+        HBoxLayout = QtWidgets.QHBoxLayout()
+        self.setLayout(HBoxLayout)
+        self.setTitle(sensitive_attribute.capitalize())
+        self.setStyleSheet("QGroupBox#NoBorderGroupBox {border:0;padding.top: 11;}")
+
+        individual_mode_groupBox = []
+        individual_ct_groupBox = []
+        individual_metrics_groupBox = []
+
+        # TODO Upgrade groupBox to collapsible section
+        # groupBox for each mode
+        for i, mode in enumerate(data):
+            individual_mode_groupBox.append(QtWidgets.QGroupBox(mode.capitalize()))
+            vBoxLayout = QtWidgets.QVBoxLayout()
+            individual_mode_groupBox[i].setLayout(vBoxLayout)
+            individual_mode_groupBox[i].setObjectName("NoBorderGroupBox")
+
+            # correspondence table
+            if pl_type == "classification":
+                # Since the confusion matrix is the same for all metrics we are going to access the first cm that appears
+                first_metric = list(fairness_results[model][sensitive_attribute].keys())[0]
+                sub_groups = fairness_results[model][sensitive_attribute][first_metric][mode]
+                individual_ct_groupBox.append(CorrespondenceTable(sub_groups))
+
+            # metrics table
+            metrics = fairness_results[model][sensitive_attribute]
+            individual_metrics_groupBox.append(FairnessMetricsTable(metrics, mode, pl_type))
+
+            # add to layout
+            if len(individual_ct_groupBox) > 0:
+                vBoxLayout.addWidget(individual_ct_groupBox[i])
+            vBoxLayout.addWidget(individual_metrics_groupBox[i])
+            vBoxLayout.addStretch()
+            HBoxLayout.addWidget(individual_mode_groupBox[i])
+
+
+class CorrespondenceTable(QtWidgets.QGroupBox):
+    def __init__(self, sub_groups):
+        """
+        Generates a table with the confusion matrix
+
+        Parameters
+        ----------
+        sub_groups: dict[sub_group -> confusion_matrix]
+            Dictionary where 'confusion_matrix' is used as a key to access the sub_group's confusion_matrix
+        """
+        super(CorrespondenceTable, self).__init__()
+
+        # groupBox for correspondence table
+        tableGridLayout = QtWidgets.QGridLayout()
+        self.setLayout(tableGridLayout)
+
+        # 'Class' label
+        labelClass = QtWidgets.QLabel()
+        labelClass.setText("Class")
+        labelClass.setStyleSheet("font-weight: bold")
+        tableGridLayout.addWidget(labelClass, 0, 0, alignment=Qt.AlignCenter)
+
+        for j, sub_group in enumerate(sub_groups):
+            # subgroup labels
+            labelSubgroup = QtWidgets.QLabel()
+            labelSubgroup.setText(str(sub_group).capitalize())
+            tableGridLayout.addWidget(labelSubgroup, 0, j + 1, alignment=Qt.AlignCenter)
+
+            confusion_matrix = sub_groups[sub_group]['confusion_matrix']
+            n = int(np.sqrt(len(confusion_matrix)))
+            cm = np.array(confusion_matrix).reshape(n, n)
+
+            values = np.sum(cm, axis=0)
+            for k, total in enumerate(values):
+                # classes labels
+                labelClass = QtWidgets.QLabel()
+                labelClass.setText(str(k))
+                labelClass.setStyleSheet("font-weight: bold")
+                tableGridLayout.addWidget(labelClass, k + 1, 0, alignment=Qt.AlignCenter)
+                # values labels 
+                labelValue = QtWidgets.QLabel()
+                labelValue.setText(str(total))
+                labelValue.setStyleSheet("font-weight: bold")
+                tableGridLayout.addWidget(labelValue, k + 1, j + 1, alignment=Qt.AlignCenter)
+
+
+class FairnessMetricsTable(QtWidgets.QGroupBox):
+    def __init__(self, metrics, mode, pl_type):
+        """
+        Generates a table with the given metrics
+
+        Parameters
+        ----------
+        metrics: dict[metric -> value]
+
+        mode: {'train', 'test'}
+            Which mode should be accessed.
+
+        pl_type: {'classification', 'regression}
+            Which type of prediction task is tackled by the pipeline.
+        """
+        super(FairnessMetricsTable, self).__init__()
+
+        tableGridLayout = QtWidgets.QGridLayout()
+        self.setLayout(tableGridLayout)
+
+        # 'metric' label
+        labelMetric = QtWidgets.QLabel()
+        labelMetric.setText("Metric")
+        labelMetric.setStyleSheet("font-weight: bold")
+        tableGridLayout.addWidget(labelMetric, 0, 0, alignment=Qt.AlignCenter)
+
+        if pl_type == "regression":
+            # 'value' labels
+            labelValue = QtWidgets.QLabel()
+            labelValue.setText("Value")
+            labelValue.setStyleSheet("font-weight: bold")
+            tableGridLayout.addWidget(labelValue, 0, 1, alignment=Qt.AlignCenter)
+
+        for j, metric in enumerate(metrics):
+            # fairness metrics labels
+            labelMetric = QtWidgets.QLabel()
+            labelMetric.setText(str(metric).capitalize())
+            labelMetric.setStyleSheet("font-weight: bold")
+            tableGridLayout.addWidget(labelMetric, j + 1, 0, alignment=Qt.AlignCenter)
+
+            values = metrics[metric][mode]
+
+            if pl_type == "classification":
+                for k, value in enumerate(values):
+                    measure = values[value]['affected_percent']
+                    # subgroup labels
+                    labelSubgroup = QtWidgets.QLabel()
+                    labelSubgroup.setText(str(value).capitalize())
+                    tableGridLayout.addWidget(labelSubgroup, 0, k + 1, alignment=Qt.AlignCenter)
+                    # fairness measures
+                    labelValue = QtWidgets.QLabel()
+                    labelValue.setText(f"{measure:.3f}")
+                    tableGridLayout.addWidget(labelValue, j + 1, k + 1, alignment=Qt.AlignCenter)
+
+            if pl_type == "regression":
+                # fairness measures
+                labelValue = QtWidgets.QLabel()
+                labelValue.setText(f"{values:.3f}")
+                tableGridLayout.addWidget(labelValue, j + 1, 1, alignment=Qt.AlignCenter)
