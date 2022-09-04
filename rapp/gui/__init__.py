06e538465b9a33ae0cdb8a4182d319e78f1f41d0
Manh Khoi Duong <duong@hhu.de>
2022-09-05 01:17:11 +0200

    Comment uncessary import

--- rapp/gui/__init__.py
+++ rapp/gui/__init__.py
@@ -2,7 +2,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMainWindow
 # theme
-from qt_material import apply_stylesheet
+# from qt_material import apply_stylesheet
 
 # dataframe
 from rapp.gui.dbview import DataView
